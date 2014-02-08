d092d014e6ae74f12414e513dfd9023bc23d32db
Benny Malengier <benny.malengier@gmail.com>
2014-02-08 16:35:52 +0100

    improvements to event code

--- sketches/Fe_cube_05_pbtn_events/Fe_cube_05_pbtn_events.ino
+++ sketches/Fe_cube_05_pbtn_events/Fe_cube_05_pbtn_events.ino
@@ -5,17 +5,17 @@ Controlling a simple led via a pushbutton
 #define buttonPin  2 // the number of the pushbutton pin
 #define ledPin    11 // a PWM pin for the LED!
 
-bool test = true;
+bool test        = false;
 // variable that changes
-bool allowInput = true;
+bool allowInput  = true;
 int btnStatePrev = LOW;
-int btnState = LOW;
+int btnState     = LOW;
 
 // events
 #define BTNFIXED     0    //no change
 #define BTNPRESS     1
 #define BTNRELEASE   2
-unsigned long timepressed = 0;
+unsigned long timepressed  = 0;
 unsigned long timereleased = 0;
 // button press types
 #define NOPRESS      0
@@ -25,13 +25,13 @@ unsigned long timereleased = 0;
 #define EXTREMEPRESS 4
 
 int presstypePrev = NOPRESS;
-int presstype = NOPRESS;
+int presstype     = NOPRESS;
 unsigned long prevpresstime = 0;
 
 //sensitivity constants
 #define SHORTPRESSMAXDURATION 2000UL
-#define LONGPRESSMAXDURATION  5000UL
-#define DOUBLEPRESSWAIT       700UL
+#define LONGPRESSMAXDURATION  4000UL   // 4 seconds
+#define DOUBLEPRESSWAIT       500UL    //half a second!
 
 
 void setup() {    
@@ -44,7 +44,7 @@ void setup() {
 }
 
 void inputsampling(){
-  //read the button if allowed
+  //read the pushbutton if allowed
   if (allowInput) {
     if (presstype == NOPRESS) {
       btnStatePrev = btnState;
@@ -82,11 +82,18 @@ void  eventhandling(){
   }
   switch (event) {
     case BTNFIXED:
-      // if no event, we can have a long press
+      // if no event, we can have a long press or certain short press
       if (timereleased > 0 && timepressed > timereleased
           && currt - timepressed > LONGPRESSMAXDURATION){
         presstype = EXTREMEPRESS;
-      }
+        presstypePrev = NOPRESS;
+        //avoid double handling of same events
+        timepressed = timereleased;
+      } else if (timereleased > 0 && presstypePrev == SHORTPRESS 
+                  && currt - timereleased > DOUBLEPRESSWAIT){
+        presstype = SHORTPRESS;
+        presstypePrev = NOPRESS;
+      } 
       break;
     case BTNPRESS:
       // button is pressed in, we don't sent a press type for this
@@ -99,9 +106,10 @@ void  eventhandling(){
             && (timepressed - prevtimereleased) < DOUBLEPRESSWAIT) {
           //a double press
           presstype = DOUBLEPRESS;
+          presstypePrev = NOPRESS;
         } else {
-          //a normal short press
-          presstype = SHORTPRESS;
+          //a normal short press, store, will be handled if no double press
+          presstype = NOPRESS;
           //remember it in case it will be double press:
           presstypePrev = SHORTPRESS;
         }
@@ -111,14 +119,16 @@ void  eventhandling(){
       } // no need for extremelong test, BTNFIXED handles it
   }
   if (test && event != BTNFIXED){
-    Serial.print(presstype);Serial.print(" "); Serial.println(timepressed - prevtimereleased);
+    Serial.print(presstype);Serial.print(" ");
+    Serial.println(timepressed - prevtimereleased);
   }
-  // presstype has been set. The application should process it and set it to NOPRESS
+  // presstype has been set. The app should process it and set it to NOPRESS
 }
 
+// our app: a led that burns, dims and flickers
 unsigned long looptime = 0;
-#define LEDOFF 0
-#define LEDON  1
+#define LEDOFF     0
+#define LEDON      1
 #define LEDFLICKER 2
 int ledstate = LEDOFF;
 unsigned long flickertime = 0;
@@ -136,6 +146,7 @@ void loop(){
   // handle button press if present
   switch (presstype) {
     case LONGPRESS:
+    case EXTREMEPRESS:
       //switch on or off
       if (ledstate == LEDOFF){
          analogWrite(ledPin, 255);
@@ -169,15 +180,12 @@ void loop(){
   //if flicker, we flicker once every half second
   if (ledstate == LEDFLICKER) {
     if (currt - flickertime == 0) {
-      //on
-      analogWrite(ledPin, dimvalue*4);
+      analogWrite(ledPin, dimvalue*4); //on
     } else if (currt - flickertime > 1000) {
-      //on and restart a cycle
-      analogWrite(ledPin, dimvalue*4);
+      analogWrite(ledPin, dimvalue*4); // restart
       flickertime = currt;
     } else if (currt - flickertime > 500) {
-      //off
-      analogWrite(ledPin, 0);
+      analogWrite(ledPin, 0); //off
     }
   }
 }
